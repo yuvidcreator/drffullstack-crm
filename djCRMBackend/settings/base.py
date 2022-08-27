660cf354207cbbdb41b45dd505580f6834e7f9c3
Yuvraj Limbole <yuvrajl.103@gmail.com>
2022-08-27 22:23:30 +0530

    created superuser

--- djCRMBackend/settings/base.py
+++ djCRMBackend/settings/base.py
@@ -34,7 +34,10 @@ DEBUG = env("DEBUG")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
-# ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(",")
+# ALLOWED_HOSTS = env(
+#     "ALLOWED_HOSTS", cast=lambda hosts: [host.strip() for host in hosts.split(",")]
+# )
 ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
 
@@ -53,6 +56,7 @@ DJANGO_APPS = [
 SITE_ID = 1
 
 THIRD_PARTY_APPS = [
+    "jazzmin",
     "rest_framework",
     "djoser",
     "rest_framework_simplejwt",
@@ -191,6 +195,146 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.User"
+
+
+
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "Yantrayug Adminstration",
+    # Title on the brand, and login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "Yantrayug Admin",
+    # Logo to use for your site, must be present in static files, used for brand on top left
+    "site_logo": "logo/YantrayugLogo.png",
+    # CSS classes that are applied to the logo above
+    "site_logo_classes": "img-circle",
+    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+    "site_icon": None,
+    # Welcome text on the login screen
+    "welcome_sign": "Welcome to the Yantrayug",
+    # Copyright on the footer
+    "copyright": "Yantrayug Hero Hub",
+    # Field name on user model that contains avatar image
+    "user_avatar": None,
+    #############
+    # Side Menu #
+    #############
+    # Whether to display the side menu
+    "show_sidebar": True,
+    # Whether to aut expand the menu
+    "navigation_expanded": True,
+    # Hide these apps when generating side menu e.g (auth)
+    "hide_apps": [],
+    # Hide these models when generating side menu (e.g auth.user)
+    "hide_models": [],
+    # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
+    # "order_with_respect_to": ["auth", "books", "books.author", "books.book"],
+    "order_with_respect_to": ["user"],
+    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
+    # for the full list of 5.13.0 free icon classes
+    # <i class="fas fa-globe-europe"></i>
+    "icons": {
+        # Auth
+        "account.User": "fas fa-user",
+        "auth.Group": "fas fa-users",
+        "home.CircuitPackage": "fas fa-box-open",
+        # Home
+        # "home.Comment": "fas fa-comments",
+        # "home.Continent": "fas fa-globe-europe",
+        # "home.Country": "fas fa-flag",
+        # "home.EveryYantrayugInclude": "fa fa-wrench",
+        # "home.Offer": "fa fa-gift",
+        # "home.PropertyAddOn": "fas fa-plus",
+        # "home.PropertyRules": "fa fa-gavel",
+        # "home.PropertyImages": "fas fa-image",
+        # "home.Property": "fa fa-home",
+        # "home.PropertyBooking": "fas fa-pen",
+        # "home.PropertyExperience": "fas fa-user-tie",
+        # "home.State": "fas fa-globe-africa",
+        # "home.StateExtraInfo": "fas fa-city",
+        # "home.YantrayugHomeExperience": "fas fa-user-tie",
+        # "home.Tag": "fas fa-tag",
+        # Portal
+        # "portal.Address": "fas fa-address-card",
+        # "portal.ContactInfo": "fas fa-file-signature",
+        # "portal.FAQ": "fa fa-question-circle",
+        # "portal.Investor": "fas fa-comment-dollar",
+        # "portal.Page": "far fa-file",
+        # "portal.PrivacyPolicy": "fas fa-lock",
+        # "portal.SocialLink": "fa fa-link",
+        # "portal.SubscribeNewsLetter": "fa fa-rocket",
+        # "portal.Team": "fas fa-users-cog",
+        # "portal.TermsCondition": "fa fa-gavel",
+    },
+    # Icons that are used when one is not manually specified
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-globe-europe",
+    #################
+    # Related Modal #
+    #################
+    # Use modals instead of popups
+    "related_modal_active": False,
+    #############
+    # UI Tweaks #
+    #############
+    # Relative paths to custom CSS/JS scripts (must be present in static files)
+    "custom_css": None,
+    "custom_js": None,
+    # Whether to show the UI customizer on the sidebar
+    "show_ui_builder": True,
+    ###############
+    # Change view #
+    ###############
+    # Render out the change view as a single form, or in tabs, current options are
+    # - single
+    # - horizontal_tabs (default)
+    # - vertical_tabs
+    # - collapsible
+    # - carousel
+    "changeform_format": "horizontal_tabs",
+    # override change forms on a per modeladmin basis
+    "changeform_format_overrides": {
+        "auth.user": "horizontal_tabs",
+        "auth.group": "horizontal_tabs",
+    },
+    # how many objects show er page
+    "list_per_page": env("LIST_PER_PAGE", cast=int, default=20),
+}
+
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": True,
+    "brand_small_text": False,
+    "brand_colour": False,
+    # "accent": "accent-lime",
+    # "navbar": "navbar-dark",
+    "no_navbar_border": True,
+    "navbar_fixed": True,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": True,
+    # "sidebar": "sidebar-dark-primary",
+    # "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": True,
+    "sidebar_nav_child_indent": True,
+    "sidebar_nav_compact_style": True,
+    "sidebar_nav_legacy_style": True,
+    "sidebar_nav_flat_style": True,
+    # "theme": "solar",
+    # "dark_mode_theme": "darkly",
+    # "button_classes": {
+    #     "primary": "btn-primary",
+    #     "secondary": "btn-secondary",
+    #     "info": "btn-outline-info",
+    #     "warning": "btn-outline-warning",
+    #     "danger": "btn-outline-danger",
+    #     "success": "btn-outline-success"
+    # },
+    "actions_sticky_top": True,
+}
+
+
 
 
 REST_FRAMEWORK = {
